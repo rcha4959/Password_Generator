ddc787d43e9ae7366bf129259ae47ceb9260c8aa
Rakesh Chandra <rakesh.ragamalika@gmail.com>
2020-08-29 19:49:04 +1000

    adding updated JS file

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = lowerCase.toUpperCase();
+var specialCharacters = "!@#$%^&*()";
+var numbers = "1234567890";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = createPassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function createPassword(){
+var passWordLength = prompt("Enter the length of the password");
+var haslowerCase = confirm("Please confirm password has uppercase");
+var hasUpperCase = confirm("Please confirm password has lowercase");
+var hasspecialCharacters = confirm("Please confirm password has special characters");
+var hasNumbers = confirm("Please confirm password has numbers");
+
+
+
+
+var result = ""
+
+if (passWordLength >= 8 && passWordLength <= 128) {
+  var charpool = lowerCase;
+  if (hasUpperCase) {
+    charpool += upperCase;
+  }if (hasspecialCharacters) {
+    charpool += specialCharacters
+    
+  }if (hasNumbers) {
+    charpool += numbers
+  }
+}
+
+for (var i = 0; i < passWordLength; i++) {
+  var index = Math.floor(Math.random() * charpool.length);
+  result += charpool[index]
+  
+}
+  return result;
+}
